999cd6e6e1c8857d7ec101bbfb8f829f7a97586e
iris-pascoal <65465221+iris-pascoal@users.noreply.github.com>
2022-06-02 15:26:33 +0100

    Update smart_leds_esp-v.1.ino

--- smart_leds_esp/smart_leds_esp-v.1.ino
+++ smart_leds_esp/smart_leds_esp-v.1.ino
@@ -13,23 +13,23 @@ PubSubClient client(wifi_client);
 char ssid[] = SECRET_SSID;       
 char pass[] = SECRET_PASS; 
 
-BlockNot sectionTimer(6, SECONDS);
-BlockNot shelfTimer(6,  SECONDS);
+BlockNot sectionTimer(2, SECONDS);
+BlockNot shelfTimer(2,  SECONDS);
 
 ///////////////////////////////////////////////////////
-//.........MAIN LED STRIP INITIALIZATION.............//
+//.........MAIN LED STRIP INITIALIZATION.............//  ---------   the main led strips consists of a strip above the closets and one strip below the closets that act as copies
 ///////////////////////////////////////////////////////
 #define LED_PIN     14 //main led strip pin 
-#define NUM_LEDS    173 //total number of leds in the main strip
-#define NUM_SEC     6 //total number of led sections
+#define NUM_LEDS    160//total number of leds in the main strip ->  80cm/0,5cm = 160leds
+#define NUM_SEC     2 //total number of led sections
 
 CRGB leds[NUM_LEDS];//main led strip controller
 CRGB moodColor_aux;//color control for mood service
-int moodColor[6][3] = {{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255}};//color associated withe ach state
+int moodColor[6][3] = {{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255}};//color associated withe Each state
 int secColor[3] = {255,255,255};//color control for main strip sections
 boolean moodFlag = false;
 
-int main_led_map[] = {26, 30, 28, 29, 29, 31};//number of leds per section: 0-if section is off; 1- if section is on
+int main_led_map[] = {80,80};//number of leds per section: 0-if section is off; 1- if section is on
 int section = -1; // section of leds being used
 
 ///////////////////////////////////////////////////////
@@ -37,15 +37,26 @@ int section = -1; // section of leds being used
 ///////////////////////////////////////////////////////
 #define LED_PIN_1 22
 #define LED_PIN_2 33
-#define NUM_STRIPS 2 //two led strips connected in series
-#define NUM_LEDS_PER_STRIP 36
+#define LED_PIN_4 22//?????????????????????????????????????????????????
+#define LED_PIN_5 33//??????????????????????????????????????????????????????????
+#define NUM_STRIPS 4 //two led strips connected in series
+#define NUM_LEDS_PER_STRIP 3 //?????????????????????????????????????????????????
 #define NUM_LEDS_1 74
-#define NUM_SEC_1 8
+#define NUM_SEC_1 16
 
 CRGB leds_1[NUM_LEDS_1]; // 2 led strips
-int shelfColor[NUM_SEC_1][3] = {{255,255,255}, {255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255}};
-int sec_led_map[NUM_SEC_1] = {9,9,9,9,9,9,9,9};//number of leds per section: 
+int shelfColor[NUM_SEC_1][3] = {{255,255,255}, {255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255},{255,255,255}};//?????????????????????????????????
+int sec_led_map[NUM_SEC_1] = {9,9,9,9,9,9,9,9};//number of leds per section: ????????????????????????????????????????????????????????????????????????????????????????''
 int section_1 = -1; // section of leds being used
+
+///////////////////////////////////////////////////////
+//....LOWER CABINET LED STRIP INITIALIZATION.........//
+///////////////////////////////////////////////////////
+#define LED_PIN_3    3//?????????????????????????????????????????
+#define NUM_LEDS_3   120//total number of leds 
+#define NUM_SEC_3     1 //total number of led sections
+
+CRGB leds_3[NUM_LEDS_3];//led strip controller
 
 
 //////////////////////////////////////////////////////
@@ -64,6 +75,8 @@ const char* topicMoodSection = "mood/section";//subscribe
 const char* topicMoodSectionColor = "mood/section/color";//subscribe
 const char* topicShelf1 = "shelf1";//subscribe
 const char* topicShelf2 = "shelf2";//subscribe
+const char* topicShelf3 = "shelf3";//publish
+const char* topicShelf4 = "shelf4";//publish
 const char* topicShelfColor_0= "shelf/color/0";//subscribe
 const char* topicShelfColor_1= "shelf/color/1";//subscribe
 const char* topicShelfColor_2= "shelf/color/2";//subscribe
@@ -72,7 +85,17 @@ const char* topicShelfColor_4= "shelf/color/4";//subscribe
 const char* topicShelfColor_5= "shelf/color/5";//subscribe
 const char* topicShelfColor_6= "shelf/color/6";//subscribe
 const char* topicShelfColor_7= "shelf/color/7";//subscribe
+const char* topicShelfColor_8= "shelf/color/8";//subscribe
+const char* topicShelfColor_9= "shelf/color/9";//subscribe
+const char* topicShelfColor_10= "shelf/color/10";//subscribe
+const char* topicShelfColor_11= "shelf/color/11";//subscribe
+const char* topicShelfColor_12= "shelf/color/12";//subscribe
+const char* topicShelfColor_13= "shelf/color/13";//subscribe
+const char* topicShelfColor_14= "shelf/color/14";//subscribe
+const char* topicShelfColor_15= "shelf/color/15";//subscribe
 const char* acdTopic = "automatic_cabinet_door/controls";//subscribe
+const char* topicVoiceCommand = "voice_control/status";//subscribe
+
 
 
 
@@ -92,10 +115,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
   
   if(strcmp(topic,topicMoodSection) == 0){//mood sections topic  
     CRGB sec_color = CRGB(secColor[0], secColor[1], secColor[2]);
-
     int len = strlen(payloadChar);
     client.publish("mood/section/status", payloadChar, true);
-
     sectionTimer.RESET;
     
     
@@ -141,7 +162,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   if(strcmp(topic, topicMoodStatus)==0){//led moods status
     CRGB aux_color;
     client.publish("mood/state/status", payloadChar, true);
-    
+    moodFlag = true; //assim nã é preciso ligar o mood primeiro
     if(strcmp(payloadChar, "1")==0){ //neutral mood
       Serial.println("Mood service ON"); 
       //Serial.println("waiting for input......"); 
@@ -259,7 +280,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     CRGB aux_color;
     shelfTimer.RESET;
     client.publish("shelf1/status", payloadChar, true);
-
     
       if (strcmp(payloadChar, "1")==0) {
         Serial.println("WINE");
@@ -267,17 +287,17 @@ void callback(char* topic, byte* payload, unsigned int length) {
         handle_led_sec(0, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       } 
       if(strcmp(payloadChar, "2")==0){
-         Serial.println("GIN");
+         Serial.println("COOKIES");
          aux_color = CRGB(shelfColor[1][0], shelfColor[1][1], shelfColor[1][2]);
         handle_led_sec(1, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       }
       if(strcmp(payloadChar, "3")==0){
-        Serial.println("COOKIES");
+        Serial.println("APPLES");
          aux_color = CRGB(shelfColor[2][0], shelfColor[2][1], shelfColor[2][2]);
         handle_led_sec(2, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       } 
       if(strcmp(payloadChar, "4")==0){
-        Serial.println("APPLES");
+        Serial.println("PASTA");
          aux_color = CRGB(shelfColor[3][0], shelfColor[3][1], shelfColor[3][2]);
         handle_led_sec(3, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       }
@@ -290,28 +310,89 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
     if(strcmp(topic, topicShelf2) == 0){
       client.publish("shelf2/status", payloadChar, true);
-
       CRGB aux_color;
       shelfTimer.RESET;
       if(strcmp(payloadChar, "1")==0){
-        Serial.println("PASTA");
+        Serial.println("KETCHUP");
         aux_color = CRGB(shelfColor[4][0], shelfColor[4][1], shelfColor[4][2]);
         handle_led_sec(4, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       } 
       if(strcmp(payloadChar, "2")==0){
-        Serial.println("KETCHUP");
+        Serial.println("TOMATOES");
         aux_color = CRGB(shelfColor[5][0], shelfColor[5][1], shelfColor[5][2]);
         handle_led_sec(5, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       }  
       if(strcmp(payloadChar, "3")==0){
-        Serial.println("RICE");
+        Serial.println("BEANS");
         CRGB aux_color = CRGB(shelfColor[6][0], shelfColor[6][1], shelfColor[6][2]);
         handle_led_sec(6, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       } 
       if(strcmp(payloadChar, "4")==0){
-        Serial.println("WATER");
+        Serial.println("SUGAR");
         aux_color = CRGB(shelfColor[7][0], shelfColor[7][1], shelfColor[7][2]);
         handle_led_sec(7, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "0")==0){
+        Serial.println("OFF");
+        aux_color = CRGB(0,0,0);
+        handle_led_sec(section_1, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+        section_1=-1;
+      } 
+    }
+    if(strcmp(topic, topicShelf3) == 0){
+      client.publish("shelf3/status", payloadChar, true);
+      CRGB aux_color;
+      shelfTimer.RESET;
+      if(strcmp(payloadChar, "1")==0){
+        Serial.println("SALT");
+        aux_color = CRGB(shelfColor[8][0], shelfColor[8][1], shelfColor[8][2]);
+        handle_led_sec(8, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "2")==0){
+        Serial.println("CANDY");
+        aux_color = CRGB(shelfColor[9][0], shelfColor[9][1], shelfColor[9][2]);
+        handle_led_sec(9, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      }  
+      if(strcmp(payloadChar, "3")==0){
+        Serial.println("PEPPER");
+        CRGB aux_color = CRGB(shelfColor[10][0], shelfColor[10][1], shelfColor[10][2]);
+        handle_led_sec(10, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "4")==0){
+        Serial.println("COKE");
+        aux_color = CRGB(shelfColor[11][0], shelfColor[11][1], shelfColor[11][2]);
+        handle_led_sec(11, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "0")==0){
+        Serial.println("OFF");
+        aux_color = CRGB(0,0,0);
+        handle_led_sec(section_1, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+        section_1=-1;
+      } 
+    }
+    if(strcmp(topic, topicShelf4) == 0){
+      client.publish("shelf4/status", payloadChar, true);
+      CRGB aux_color;
+      shelfTimer.RESET;
+      if(strcmp(payloadChar, "1")==0){
+        Serial.println("WATER");
+        aux_color = CRGB(shelfColor[12][0], shelfColor[12][1], shelfColor[12][2]);
+        handle_led_sec(12, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "2")==0){
+        Serial.println("BREAD");
+        aux_color = CRGB(shelfColor[13][0], shelfColor[13][1], shelfColor[13][2]);
+        handle_led_sec(13, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      }  
+      if(strcmp(payloadChar, "3")==0){
+        Serial.println("MILK");
+        CRGB aux_color = CRGB(shelfColor[14][0], shelfColor[14][1], shelfColor[14][2]);
+        handle_led_sec(14, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
+      } 
+      if(strcmp(payloadChar, "4")==0){
+        Serial.println("SODA");
+        aux_color = CRGB(shelfColor[15][0], shelfColor[15][1], shelfColor[15][2]);
+        handle_led_sec(15, aux_color, sec_led_map, NUM_LEDS_1, leds_1, 1);
       } 
       if(strcmp(payloadChar, "0")==0){
         Serial.println("OFF");
@@ -426,6 +507,179 @@ void callback(char* topic, byte* payload, unsigned int length) {
         shelfColor[7][i] = aux[i];  
       
     }
+        if(strcmp(topic,topicShelfColor_8) == 0){
+      client.publish("shelf/color/8/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[8][i] = aux[i];  
+     
+    }
+    if(strcmp(topic,topicShelfColor_9) == 0){
+      client.publish("shelf/color/9/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[9][i] = aux[i]; 
+
+      
+    }
+    if(strcmp(topic,topicShelfColor_10) == 0){
+      client.publish("shelf/color/10/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[10][i] = aux[i];  
+
+      
+    }
+    if(strcmp(topic,topicShelfColor_11) == 0){
+      client.publish("shelf/color/11/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[11][i] = aux[i];  
+      
+    }
+    if(strcmp(topic,topicShelfColor_12) == 0){
+      client.publish("shelf/color/12/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[12][i] = aux[i];  
+      
+    }
+    if(strcmp(topic,topicShelfColor_13) == 0){
+      client.publish("shelf/color/13/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[13][i] = aux[i];  
+      
+    }
+    if(strcmp(topic,topicShelfColor_14) == 0){ 
+      client.publish("shelf/color/14/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[14][i] = aux[i]; 
+      
+    }
+    if(strcmp(topic,topicShelfColor_15) == 0){
+      client.publish("shelf/color/15/status", payloadChar, true);
+      char * token = strtok(payloadChar, ",");
+      int aux[3];
+      aux[0] = atoi(token);
+      for(int i = 1; i<3; i++) {
+        token = strtok(NULL, ",");
+        aux[i] = atoi(token);
+      }
+      for(i = 0; i<3; i++)
+        shelfColor[15][i] = aux[i];  
+      
+    }
+    if(strcmp(topic,topicVoiceCommand) == 0){
+     //blinking all leds
+       for(int j = 0; j<2; j++){
+        for(int i = 0; i < NUM_LEDS; i ++){
+          leds[i] = CRGB::Blue;
+        }
+        FastLED.show();
+        delay(500);
+        for(int i = 0; i < NUM_LEDS; i ++){
+          leds[i] = CRGB::Black; 
+        }
+        FastLED.show();
+        delay(500);
+       } 
+      
+    }
+    if(strcmp(topic,acdTopic) == 0){
+     //blinking leds orange and yellow
+
+      if(strcmp(payloadChar, "Opening")==0){
+       
+        for(int i = 0; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Yellow;
+        }
+        FastLED.show();
+        delay(500);
+        for(int i = 0; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Black; 
+        }
+        FastLED.show();
+        delay(500);      
+        for(int i = 1; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Orange;
+        }
+        FastLED.show();
+        delay(500);
+        for(int i = 1; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Black; 
+        }
+        FastLED.show();
+        delay(500);
+      }  
+      if(strcmp(payloadChar, "Closing")==0){
+       
+        for(int i = 0; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Yellow;
+        }
+        FastLED.show();
+        delay(500);
+        for(int i = 0; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Black; 
+        }
+        FastLED.show();
+        delay(500);      
+        for(int i = 1; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Orange;
+        }
+        FastLED.show();
+        delay(500);
+        for(int i = 1; i < NUM_LEDS_3; i = i+2){
+          leds_3[i] = CRGB::Black; 
+        }
+        FastLED.show();
+        delay(500);
+      } 
+      
+    }
 }
 
 void reconnect() {
@@ -450,6 +704,8 @@ void reconnect() {
   client.subscribe(topicMoodSectionColor);
   client.subscribe(topicShelf1);
   client.subscribe(topicShelf2);
+  client.subscribe(topicShelf3);
+  client.subscribe(topicShelf4);
   client.subscribe(topicShelfColor_0);
   client.subscribe(topicShelfColor_1);
   client.subscribe(topicShelfColor_2);
@@ -458,7 +714,16 @@ void reconnect() {
   client.subscribe(topicShelfColor_5);
   client.subscribe(topicShelfColor_6);
   client.subscribe(topicShelfColor_7);
+  client.subscribe(topicShelfColor_8);
+  client.subscribe(topicShelfColor_9);
+  client.subscribe(topicShelfColor_10);
+  client.subscribe(topicShelfColor_11);
+  client.subscribe(topicShelfColor_12);
+  client.subscribe(topicShelfColor_13);
+  client.subscribe(topicShelfColor_14);
+  client.subscribe(topicShelfColor_15);
   client.subscribe(acdTopic);
+  client.subscribe(topicVoiceCommand);
 
 
   
@@ -481,9 +746,12 @@ void setup() {
   
  
  FastLED.addLeds<WS2812,LED_PIN , GRB>(leds, NUM_LEDS); //main led strip
+ FastLED.addLeds<WS2812,LED_PIN , GRB>(leds, NUM_LEDS); //second main led strip mirroring the first
  FastLED.addLeds<WS2812, LED_PIN_1, GRB>(leds_1,0, NUM_LEDS_PER_STRIP); //shelf leds strip
  FastLED.addLeds<WS2812, LED_PIN_2, GRB>(leds_1,NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP); //shelf leds strip
-  
+  FastLED.addLeds<WS2812, LED_PIN_4, GRB>(leds_1,NUM_LEDS_PER_STRIP*2, NUM_LEDS_PER_STRIP); //shelf leds strip
+ FastLED.addLeds<WS2812, LED_PIN_5, GRB>(leds_1,NUM_LEDS_PER_STRIP*3, NUM_LEDS_PER_STRIP); //shelf leds strip
+ FastLED.addLeds<WS2812,LED_PIN_3 , GRB>(leds_3, NUM_LEDS_3); //lower cabinets led strip
   Serial.print("\n\r Initializing Wifi \n\r");
   
   WiFi.begin(ssid,pass);
@@ -536,7 +804,6 @@ void loop() {
   handle_led_sec(section,moodColor_aux , main_led_map, NUM_LEDS, leds, 0);
   Serial.println("timer 1 off");
   section = -1;
-
  }
  if(shelfTimer.TRIGGERED){
   handle_led_sec(section_1, CRGB::Black, sec_led_map, NUM_LEDS_1, leds_1, 1); 
